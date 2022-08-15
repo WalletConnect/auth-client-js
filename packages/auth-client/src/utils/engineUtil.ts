93d339eba26bd778b06cde7188d6a15c95b76c32
Ben Kremer <ben@walletconnect.com>
2022-08-15 19:45:54 +0200

    fix: removes unused engineUtil

--- packages/auth-client/src/utils/engineUtil.ts
+++
@@ -1,5 +0,0 @@
-// FIXME: @walletconnect/utils engineEvent is typed to SignClient Events only
-// -> make typing generic and remove this duplicated definition.
-export function engineEvent(event: string, id?: number | string | undefined) {
-  return `${event}${id ? `:${id}` : ""}`;
-}
