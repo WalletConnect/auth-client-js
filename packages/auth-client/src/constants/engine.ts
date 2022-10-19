2aef8be4c9f8ae4502748660a3adb3fb9451a46c
Ben Kremer <ben@walletconnect.com>
2022-10-19 11:55:32 +0200

    fix(rpc): re-enables expected TTLs

--- packages/auth-client/src/constants/engine.ts
+++ packages/auth-client/src/constants/engine.ts
@@ -1,38 +1,39 @@
+import { ONE_DAY, THIRTY_SECONDS } from "@walletconnect/time";
 import { JsonRpcTypes, RpcOpts } from "../types";
 
 export const ENGINE_RPC_OPTS: Record<JsonRpcTypes.WcMethod, RpcOpts> = {
   wc_pairingDelete: {
     req: {
-      // ttl: ONE_DAY,
+      ttl: ONE_DAY,
       prompt: false,
       tag: 1000,
     },
     res: {
-      // ttl: ONE_DAY,
+      ttl: ONE_DAY,
       prompt: false,
       tag: 1001,
     },
   },
   wc_pairingPing: {
     req: {
-      // ttl: 30,
+      ttl: THIRTY_SECONDS,
       prompt: false,
       tag: 1002,
     },
     res: {
-      // ttl: 30,
+      ttl: THIRTY_SECONDS,
       prompt: false,
       tag: 1003,
     },
   },
   wc_authRequest: {
     req: {
-      // ttl: ONE_DAY,
+      ttl: ONE_DAY,
       prompt: true,
       tag: 3000,
     },
     res: {
-      // ttl: ONE_DAY,
+      ttl: ONE_DAY,
       prompt: false,
       tag: 3001,
     },
