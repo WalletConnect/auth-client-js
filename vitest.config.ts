9c341857869228a3cd4e086725a7b38e1044c1e1
Gancho Radkov <ganchoradkov93@gmail.com>
2022-11-25 15:37:31 +0200

    fix: rm test env params during testing as it overwrites env params

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from "vitest/config";
-import testEnv from "./testEnv.json";
 
 export default defineConfig({
   test: {
-    env: {
-      ...testEnv,
-    },
     testTimeout: 10_000,
     hookTimeout: 10_000,
   },
