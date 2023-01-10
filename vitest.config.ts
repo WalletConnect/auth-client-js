2212bd17527d6429f849def9fc943e50d39c5ffd
Ilja <idaderko@gmail.com>
2023-01-10 13:03:55 +0200

    add error about node version requirement for tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,12 @@
 import { defineConfig } from "vitest/config";
 
+// Node.js 16 is required to run the tests
+const versionRegex = new RegExp(`^${16}\\..*`);
+const versionCorrect = process.versions.node.match(versionRegex);
+if (!versionCorrect) {
+  throw Error(`Running on wrong Nodejs version. Please upgrade the node runtime to version ${16}`);
+}
+
 export default defineConfig({
   test: {
     testTimeout: 10_000,
